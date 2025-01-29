ae72f7fbf5cbe453c215bdb7bf8b6173f395bd78
Ian Wigley <ian.wigley@outlook.com>
2025-01-29 18:44:00 +0000

    Create ConfigureSettings.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/ConfigureSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace BinToAssembly
+{
+    public partial class ConfigureSettings : Form
+    {
+        public ConfigureSettings(SettingsCache sc)
+        {
+            InitializeComponent();
+            VasmLocation.Text = sc.VasmLocation;
+            Processor.Text = sc.Processor;
+            Kickhunk.Text = sc.Kickhunk;
+            Fhunk.Text = sc.Fhunk;
+            Flag.Text = sc.Flag;
+            Destination.Text = sc.Destination;
+        }
+
+        private void CloseTheSettings(object sender, System.EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}

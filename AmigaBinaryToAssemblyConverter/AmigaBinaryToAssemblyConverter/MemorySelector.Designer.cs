86c592fd995ddce0da8d5f311d9ba909c193ccb7
Ian Wigley <ian.wigley@outlook.com>
2025-02-08 18:49:12 +0000

    Update MemorySelector.Designer.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.Designer.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.Designer.cs
@@ -52,9 +52,9 @@
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // groupBox1
-            // 
+            //
             this.groupBox1.Controls.Add(this.startAddress8);
             this.groupBox1.Controls.Add(this.startAddress7);
             this.groupBox1.Controls.Add(this.startAddress6);
@@ -69,45 +69,45 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select Memory Start Address";
-            // 
+            //
             // startAddress4
-            // 
+            //
             this.startAddress4.DropDownWidth = 30;
             this.startAddress4.FormattingEnabled = true;
             this.startAddress4.Location = new System.Drawing.Point(105, 30);
             this.startAddress4.Name = "startAddress4";
             this.startAddress4.Size = new System.Drawing.Size(30, 21);
             this.startAddress4.TabIndex = 4;
-            // 
+            //
             // startAddress3
-            // 
+            //
             this.startAddress3.DropDownWidth = 30;
             this.startAddress3.FormattingEnabled = true;
             this.startAddress3.Location = new System.Drawing.Point(72, 30);
             this.startAddress3.Name = "startAddress3";
             this.startAddress3.Size = new System.Drawing.Size(30, 21);
             this.startAddress3.TabIndex = 3;
-            // 
+            //
             // startAddress2
-            // 
+            //
             this.startAddress2.DropDownWidth = 30;
             this.startAddress2.FormattingEnabled = true;
             this.startAddress2.Location = new System.Drawing.Point(39, 30);
             this.startAddress2.Name = "startAddress2";
             this.startAddress2.Size = new System.Drawing.Size(30, 21);
             this.startAddress2.TabIndex = 2;
-            // 
+            //
             // startAddress1
-            // 
+            //
             this.startAddress1.DropDownWidth = 30;
             this.startAddress1.FormattingEnabled = true;
             this.startAddress1.Location = new System.Drawing.Point(6, 30);
             this.startAddress1.Name = "startAddress1";
             this.startAddress1.Size = new System.Drawing.Size(30, 21);
             this.startAddress1.TabIndex = 1;
-            // 
+            //
             // groupBox2
-            // 
+            //
             this.groupBox2.Controls.Add(this.endAddress8);
             this.groupBox2.Controls.Add(this.endAddress7);
             this.groupBox2.Controls.Add(this.endAddress6);
@@ -122,41 +122,41 @@
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Select Memory End Address";
-            // 
+            //
             // endAddress4
-            // 
+            //
             this.endAddress4.FormattingEnabled = true;
             this.endAddress4.Location = new System.Drawing.Point(104, 29);
             this.endAddress4.Name = "endAddress4";
             this.endAddress4.Size = new System.Drawing.Size(30, 21);
             this.endAddress4.TabIndex = 8;
-            // 
+            //
             // endAddress3
-            // 
+            //
             this.endAddress3.FormattingEnabled = true;
             this.endAddress3.Location = new System.Drawing.Point(71, 29);
             this.endAddress3.Name = "endAddress3";
             this.endAddress3.Size = new System.Drawing.Size(30, 21);
             this.endAddress3.TabIndex = 7;
-            // 
+            //
             // endAddress2
-            // 
+            //
             this.endAddress2.FormattingEnabled = true;
             this.endAddress2.Location = new System.Drawing.Point(38, 29);
             this.endAddress2.Name = "endAddress2";
             this.endAddress2.Size = new System.Drawing.Size(30, 21);
             this.endAddress2.TabIndex = 6;
-            // 
+            //
             // endAddress1
-            // 
+            //
             this.endAddress1.FormattingEnabled = true;
             this.endAddress1.Location = new System.Drawing.Point(5, 29);
             this.endAddress1.Name = "endAddress1";
             this.endAddress1.Size = new System.Drawing.Size(30, 21);
             this.endAddress1.TabIndex = 5;
-            // 
+            //
             // button1
-            // 
+            //
             this.button1.Location = new System.Drawing.Point(102, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -164,77 +164,77 @@
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
-            // 
+            //
             // startAddress5
-            // 
+            //
             this.startAddress5.DropDownWidth = 30;
             this.startAddress5.FormattingEnabled = true;
             this.startAddress5.Location = new System.Drawing.Point(138, 30);
             this.startAddress5.Name = "startAddress5";
             this.startAddress5.Size = new System.Drawing.Size(30, 21);
             this.startAddress5.TabIndex = 5;
-            // 
+            //
             // startAddress6
-            // 
+            //
             this.startAddress6.DropDownWidth = 30;
             this.startAddress6.FormattingEnabled = true;
             this.startAddress6.Location = new System.Drawing.Point(171, 30);
             this.startAddress6.Name = "startAddress6";
             this.startAddress6.Size = new System.Drawing.Size(30, 21);
             this.startAddress6.TabIndex = 6;
-            // 
+            //
             // startAddress7
-            // 
+            //
             this.startAddress7.DropDownWidth = 30;
             this.startAddress7.FormattingEnabled = true;
             this.startAddress7.Location = new System.Drawing.Point(204, 30);
             this.startAddress7.Name = "startAddress7";
             this.startAddress7.Size = new System.Drawing.Size(30, 21);
             this.startAddress7.TabIndex = 7;
-            // 
+            //
             // startAddress8
-            // 
+            //
             this.startAddress8.DropDownWidth = 30;
             this.startAddress8.FormattingEnabled = true;
             this.startAddress8.Location = new System.Drawing.Point(237, 30);
             this.startAddress8.Name = "startAddress8";
             this.startAddress8.Size = new System.Drawing.Size(30, 21);
             this.startAddress8.TabIndex = 8;
-            // 
+            //
             // endAddress8
-            // 
+            //
             this.endAddress8.FormattingEnabled = true;
             this.endAddress8.Location = new System.Drawing.Point(235, 29);
             this.endAddress8.Name = "endAddress8";
             this.endAddress8.Size = new System.Drawing.Size(30, 21);
             this.endAddress8.TabIndex = 12;
-            // 
+            //
             // endAddress7
-            // 
+            //
             this.endAddress7.FormattingEnabled = true;
             this.endAddress7.Location = new System.Drawing.Point(203, 29);
             this.endAddress7.Name = "endAddress7";
             this.endAddress7.Size = new System.Drawing.Size(30, 21);
             this.endAddress7.TabIndex = 11;
-            // 
+            //
             // endAddress6
-            // 
+            //
             this.endAddress6.FormattingEnabled = true;
             this.endAddress6.Location = new System.Drawing.Point(170, 29);
             this.endAddress6.Name = "endAddress6";
             this.endAddress6.Size = new System.Drawing.Size(30, 21);
             this.endAddress6.TabIndex = 10;
-            // 
+            //
             // endAddress5
-            // 
+            //
             this.endAddress5.FormattingEnabled = true;
             this.endAddress5.Location = new System.Drawing.Point(137, 29);
             this.endAddress5.Name = "endAddress5";
             this.endAddress5.Size = new System.Drawing.Size(30, 21);
             this.endAddress5.TabIndex = 9;
-            // 
+            //
             // MemorySelector
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(290, 212);

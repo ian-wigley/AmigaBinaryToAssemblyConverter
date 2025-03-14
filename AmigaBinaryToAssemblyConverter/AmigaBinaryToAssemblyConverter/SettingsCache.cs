3b45c57a326011d1e65d63f9b729b35d05e230d5
Ian Wigley <ian.wigley@outlook.com>
2025-03-14 19:18:42 +0000

    Update SettingsCache.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/SettingsCache.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/SettingsCache.cs
@@ -8,7 +8,8 @@
             string kickhunk,
             string fhunk,
             string flag,
-            string destination
+            string folder,
+            string filename
             )
         {
             VasmLocation = vasmLocation;
@@ -16,13 +17,15 @@
             Kickhunk = kickhunk;
             Fhunk = fhunk;
             Flag = flag;
-            Destination = destination;
+            Folder = folder;
+            Filename = filename;
         }
         public string VasmLocation { get; private set; }
         public string Processor { get; private set; }
         public string Kickhunk { get; private set; }
         public string Fhunk { get; private set; }
         public string Flag { get; private set; }
-        public string Destination { get; private set; }
-    }
+        public string Folder { get; private set; }
+        public string Filename { get; private set; }
+        }
 }

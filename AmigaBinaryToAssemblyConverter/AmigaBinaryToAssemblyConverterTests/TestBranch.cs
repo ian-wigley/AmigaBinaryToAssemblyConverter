3cdf850a74f0665c6365c910d3484d9d1291d85f
Ian Wigley <ian.wigley@outlook.com>
2025-03-15 18:35:52 +0000

    Create TestBranch.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverterTests/TestBranch.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiBinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class TestBranch
+    {
+        [TestMethod]
+        public void TestDetail()
+        {
+
+        }
+    }
+}

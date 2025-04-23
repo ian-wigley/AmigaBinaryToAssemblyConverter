0a1b7cef15a6f0eef88d57b8630c4e0525f1b667
Ian Wigley <ian.wigley@outlook.com>
2025-04-23 18:42:34 +0100

    Create TestBinaryConverter.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverterTests/TestBinaryConverter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinToAssembly;
+
+namespace MultiBinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class TestBinaryConverter
+    {
+        [TestMethod]
+        public void TestConvertToDataDCBClick()
+        {
+            AssemblyCreator assemblyCreator = new AssemblyCreator();
+            Assert.IsNotNull(assemblyCreator);
+        }
+    }
+}
